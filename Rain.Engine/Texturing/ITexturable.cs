88880e59e9c08e893f571c8a8181f8c17350da79
Evan Overman <evanrileyoverman@gmail.com>
2022-06-25 15:21:23 -0700

    Add Texture array to ITexturable

--- Rain.Engine/Texturing/ITexturable.cs
+++ Rain.Engine/Texturing/ITexturable.cs
@@ -2,16 +2,19 @@ using Rain.Engine.Geometry;
 
 namespace Rain.Engine.Texturing;
 
-public interface ITexturable : IModel
+public interface ITexturable : IModel, IDisposable
 {
 	/// <summary> A two dimensional array representing the <c>ITexturable</c>'s faces.  </summary>
 	/// <remarks> The first index is the face number, and the second is the elements it is comprised of. </remarks>
 	uint[,] Faces { get; }
 
 	/// <summary> An array of <c>Texture</c>s to be used by the <c>IRenderable</c>. </summary>
-	/// <remarks>
-	/// The index correlates to the first index of <c>IRenderable.Faces</c>. For an untextured face use 
-	/// <c>Texture.Empty()</c>.
-	/// </remarks>
+	/// <remarks> A duplicate <c>Texture</c> instance should never be present, see <c>TextureIndices</c>. </remarks>
 	Texture[] Textures { get; }
+
+	/// <summary> An array with indices to elements in <c>Textures</c>. </summary>
+	/// <remarks> 
+	/// Index correlates with the first dimension of <c>Faces</c>, and the value is an index for <c>Textures</c>.
+	/// </remarks>
+	int[] TextureIndices { get; }
 }
