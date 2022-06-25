8b1eba9d99f54e62aa90264b4f27317ab58a1e35
Evan Overman <evanrileyoverman@gmail.com>
2022-06-25 15:21:01 -0700

    Make TexturableBase IDisposable

--- Rain.Engine/Texturing/TextureableBase.cs
+++ Rain.Engine/Texturing/TextureableBase.cs
@@ -22,5 +22,35 @@ public abstract class TexturableBase : ModelBase, ITexturable, IModel
 
 	public abstract Texture[] Textures { get; }
 
+	public abstract int[] TextureIndices { get; }
+
 	public TexturableBase(Point[] points) : base(points) { }
+
+	#region IDisposable
+
+	private bool disposed = false;
+
+	public void Dispose()
+	{
+		Dispose(true);
+		GC.SuppressFinalize(this);
+	}
+
+	protected virtual void Dispose(bool disposing)
+	{
+		if (disposed) 
+			return;
+
+		if (disposing)
+		{
+			for (var texture = 0; texture < Textures.Length; texture++)
+				Textures[texture].Dispose();
+		}
+
+		disposed = true;
+	}
+
+	~TexturableBase() => Dispose(false);
+
+	#endregion
 }
