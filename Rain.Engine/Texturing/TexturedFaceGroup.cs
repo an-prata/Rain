036da623c697333535a1c41d0747eff04a2d6534
Evan Overman <evanrileyoverman@gmail.com>
2022-07-04 18:17:07 -0700

    Textured faces now stores multiple Textures

--- Rain.Engine/Texturing/TexturedFaceGroup.cs
+++ Rain.Engine/Texturing/TexturedFaceGroup.cs
@@ -9,36 +9,28 @@ public struct TexturedFaceGroup
 		get => new()
 		{ 
 			Face = Faces[index], 
-			Texture = Textures[index] 
+			Textures = Textures[index] 
 		};
-
-		set
-		{
-			Faces[index] = value.Face;
-			textures[index] = value.Texture;
-		}
 	}
-
-	private EfficientTextureGroup textures;
 
 	public int Length { get => Faces.Length; }
 
 	public ITwoDimensional[] Faces { get; }
 
-	public EfficientTextureGroup Textures { get => textures; }
+	public EfficientTextureArrayGroup Textures { get; private set; }
 
 	public TexturedFaceGroup(TexturedFace[] texturedFaces)
 	{
-		var faces = new List<ITwoDimensional>();
-		var textures = new List<Texture>();
+		var faces = new ITwoDimensional[texturedFaces.Length];
+		var textures = new Texture[texturedFaces.Length][];
 
 		for (var face = 0; face < texturedFaces.Length; face++)
 		{
-			faces.Add(texturedFaces[face].Face);
-			textures.Add(texturedFaces[face].Texture);
+			faces[face] = texturedFaces[face].Face;
+			textures[face] = texturedFaces[face].Textures;
 		}
 
 		Faces = faces.ToArray();
-		this.textures = new(textures.ToArray());
+		Textures = new(textures.ToArray());
 	}
 }
