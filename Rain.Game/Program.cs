fd72b013a91d331cf661bee156d7d4f8988e1b59
Evan Overman <evanrileyoverman@gmail.com>
2022-07-17 15:53:55 -0700

    Update Program.cs

--- Rain.Game/Program.cs
+++ Rain.Game/Program.cs
@@ -12,39 +12,10 @@ class Program
 {
 	static void Main()
 	{
-		/* var rectangle0 = new Rectangle(new(0.25f, 0.25f, 0.0f), 0.5f, 0.5f, new(255, 255, 255));
-		rectangle0.Rotate(90.0f, Axes.Z, RotationDirection.Clockwise);
-
-		var rectangle1 = new Rectangle(new(-0.25f, -0.25f, 0.0f), 0.5f, 0.5f, new(0, 255, 255));
-		rectangle1.Rotate(90.0f, Axes.Z, RotationDirection.CounterClockwise); 
-
-		var rectangle2 = new Rectangle(new(-0.25f, 0.25f, 0.0f), 0.5f, 0.5f, new(255, 255, 255));
-
-		var rectangle3 = new Rectangle(new(0.25f, -0.25f, 0.0f), 0.5f, 0.5f, new(128, 128, 255));
-		rectangle3.Rotate(180.0f, Axes.Z);
-
-		var models = new IRenderable[] 
-		{ 
-			Solid.SolidFromFace(rectangle0, new Texture[] 
-			{ 
-				new("interesting.bmp", 0.3f), 
-				new("interesting.bmp", 0.5f), 
-				new("interesting.bmp", 0.3f), 
-				new("test.png", 1.0f)
-			}),
-			Solid.SolidFromFace(rectangle1, new Texture[] { new("greg.bmp") }), 
-			Solid.SolidFromFace(rectangle2, new Texture[] { new("suprise.bmp") }), 
-			Solid.SolidFromFace(rectangle3, new Texture[] { new("garfield.bmp") })
-		}; */
-
-		var cubeBase = new Rectangle(new(0.5f, 0.5f, -50.0f), 10.0f, 10.0f, new(203, 178, 238));
+		var cubeBase = new Rectangle(new(0.5f, 0.5f, -30.0f), 15.0f, 15.0f, new(203, 178, 238));
 		var textures = new Texture[] { new("interesting.bmp", 0.3f) };
 
-		var texturedBase = new TexturedFace()
-		{
-			Textures = new Texture[] { new("interesting.bmp", 0.3f) },
-			Face = cubeBase
-		};
+		var texturedBase = new TexturedFace(cubeBase, new Texture[] { new("interesting.bmp", 0.3f) } );
 
 		var cubeTextures = new EfficientTextureGroup[] 
 		{ 
@@ -54,7 +25,7 @@ class Program
 			new(textures)
 		};
 
-		var cube = Prism.MakePrism(texturedBase, cubeTextures, 10.0f);
+		var cube = Prism.MakePrism(texturedBase, cubeTextures, 15.0f);
 		var models = new IRenderable[] { cube };
 		var scene = new Scene(models);
 
