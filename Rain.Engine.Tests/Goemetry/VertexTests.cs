06e6f72577764bfd92c1948ec9622ef4f44847d3
Evan Overman <evanrileyoverman@gmail.com>
2022-07-17 22:26:10 -0700

    Add license headers

--- Rain.Engine.Tests/Goemetry/VertexTests.cs
+++ Rain.Engine.Tests/Goemetry/VertexTests.cs
@@ -1,3 +1,6 @@
+// Copyright (c) 2022 Evan Overman (https://an-prata.it). Licensed under the MIT License.
+// See LICENSE file in repository root for complete license text.
+
 using Xunit;
 using OpenTK.Mathematics;
 using Rain.Engine.Geometry;
