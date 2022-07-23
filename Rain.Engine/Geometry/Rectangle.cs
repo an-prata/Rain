bf4d148f011fb887c75e1ee9a3523407b86595c1
Evan Overman <evanrileyoverman@gmail.com>
2022-07-22 22:28:27 -0700

    Implement TwoDimensionalBase in Rectangle and Triangle classes

--- Rain.Engine/Geometry/Rectangle.cs
+++ Rain.Engine/Geometry/Rectangle.cs
@@ -6,53 +6,13 @@ namespace Rain.Engine.Geometry;
 /// <summary>
 /// A class for creating and manipulating rectangles.
 /// </summary>
-public class Rectangle : ITwoDimensional
+public class Rectangle : TwoDimensionalBase
 {
-	private float rotationX = 0;
+	public override uint[] Elements => new uint[] { 0, 1, 2, 0, 2, 3 };
 
-	private float rotationY = 0;
+	public override Point[] Points { get; set; }
 
-	private float rotationZ = 0;
-
-	public uint[] Elements => new uint[] { 0, 1, 2, 0, 2, 3 };
-
-	public Point[] Points { get; private set; }
-
-	public Vertex Location
-	{
-		get => GetCenterVertex();
-		set => Translate(value.X - Location.X, value.Y - Location.Y, value.Z - Location.Z);
-	}
-
-	public float Width
-	{
-		get => (float)Points[0].GetDistanceBetween(Points[1]);
-		set => Scale(value / Width, 1);
-	}
-
-	public float Height 
-	{ 
-		get => (float)Points[0].GetDistanceBetween(Points[3]);
-		set => Scale(1, value / Height); 
-	}
-
-	public float RotationX 
-	{ 
-		get => rotationX; 
-		set => Rotate(value / rotationX, Axes.X); 
-	}
-
-	public float RotationY
-	{ 
-		get => rotationY; 
-		set => Rotate(value / rotationY, Axes.Y); 
-	}
-
-	public float RotationZ 
-	{ 
-		get => rotationZ; 
-		set => Rotate(value / rotationZ, Axes.Z); 
-	}
+	public override int Sides { get => 4; }
 
 	/// <summary>
 	/// Creates a new <c>Rectangle</c> from an array of <c>Point</c> objects.
@@ -61,7 +21,8 @@ public class Rectangle : ITwoDimensional
 	/// <param name="points">
 	/// An array of <c>Point</c>s representing a rectangle.
 	/// </param>
-	public Rectangle(Point[] points)
+	public Rectangle(Point[] points) :
+		base((float)points[0].GetDistanceBetween(points[3]), (float)points[0].GetDistanceBetween(points[1]), 0.0f, 0.0f, 0.0f)
 	{
 		if (points.Length != 4)
 			throw new Exception($"{nameof(points)} is not length 4 (Given length: {points.Length}).");
@@ -91,7 +52,8 @@ public class Rectangle : ITwoDimensional
 	/// <param name="height">
 	/// The <c>Rectangle</c>'s height.
 	/// </param>
-	public Rectangle(Vertex location, float width, float height)
+	public Rectangle(Vertex location, float width, float height) :
+		base(width, height, 0.0f, 0.0f, 0.0f)
 	{
 		var halfWidth = width / 2;
 		var halfHeight = height / 2;
@@ -124,7 +86,8 @@ public class Rectangle : ITwoDimensional
 	/// <param name="color">
 	/// The <c>Rectangle</c>'s color.
 	/// </param>
-	public Rectangle(Vertex location, float width, float height, Color color)
+	public Rectangle(Vertex location, float width, float height, Color color) :
+		base(width, height, 0.0f, 0.0f, 0.0f)
 	{
 		var halfWidth = width / 2;
 		var halfHeight = height / 2;
@@ -138,109 +101,15 @@ public class Rectangle : ITwoDimensional
 		};
 	}
 
-	private Rectangle(Rectangle rectangle)
+	private Rectangle(Rectangle rectangle) :
+		base(rectangle.Width, rectangle.Height, rectangle.RotationX, rectangle.RotationY, rectangle.RotationZ)
 	{
-		rotationX = rectangle.rotationX;
-		rotationY = rectangle.rotationY;
-		rotationZ = rectangle.rotationZ;
-
 		Points = new Point[rectangle.Points.Length];
 		
 		for (var point = 0; point < Points.Length; point++)
 			rectangle.Points[point].CopyTo(out Points[point]);
 	}
 
-	public double GetDistanceBetween(ISpacial other)
-		=> (Location - other.Location).Maginitude;
-
-	public Vertex GetCenterVertex()
-	{
-		var greatestPointX = Points[0].Vertex.X;
-		var leastPointX = Points[0].Vertex.X;
-
-		var greatestPointY = Points[0].Vertex.Y;
-		var leastPointY = Points[0].Vertex.Y;
-
-		var greatestPointZ = Points[0].Vertex.Z;
-		var leastPointZ = Points[0].Vertex.Z;
-
-		for (var point = 0; point < Points.Length; point++)
-		{
-			if (Points[point].Vertex.X > greatestPointX)
-				greatestPointX = Points[point].Vertex.X;
-			else if (Points[point].Vertex.X < leastPointX)
-				leastPointX = Points[point].Vertex.X;
-
-			if (Points[point].Vertex.Y > greatestPointY)
-				greatestPointY = Points[point].Vertex.Y;
-			else if (Points[point].Vertex.Y < leastPointY)
-				leastPointY = Points[point].Vertex.Y;
-
-			if (Points[point].Vertex.Z > greatestPointZ)
-				greatestPointZ = Points[point].Vertex.Z;
-			else if (Points[point].Vertex.Z < leastPointZ)
-				leastPointZ = Points[point].Vertex.Z;
-		}
-
-		var midPointX = (greatestPointX + leastPointX) / 2;
-		var midPointY = (greatestPointY + leastPointY) / 2;
-		var midPointZ = (greatestPointZ + leastPointZ) / 2;
-
-		return new Vertex(midPointX, midPointY, midPointZ);
-	}
-
-	public void CopyTo(out ITwoDimensional twoDimensional)
+	public override void CopyTo(out TwoDimensionalBase twoDimensional)
 		=> twoDimensional = new Rectangle(this);
-
-	public void Translate(float x, float y, float z)
-		=> Points = (this * TransformMatrix.CreateTranslationMatrix(x, y, z)).Points;
-
-	public void Translate(Vertex vertex)
-		=> Translate(vertex.X, vertex.Y, vertex.Z);
-
-	public void Scale(float x, float y)
-		=> Points = (this * TransformMatrix.CreateScaleMatrix(x, y, 1)).Points;
-
-	public void Rotate(float angle, Axes axis)
-		=> Rotate(angle, axis, Location);
-
-	public void Rotate(float angle, Axes axis, RotationDirection direction)
-	{
-		if (direction == RotationDirection.CounterClockwise)
-			Rotate(angle, axis);
-		else
-			Rotate(-angle, axis);
-	}
-
-	public void Rotate(float angle, Axes axis, Vertex vertex)
-	{
-		var transform = TransformMatrix.CreateTranslationMatrix(vertex);
-		transform *= TransformMatrix.CreateRotationMatrix(angle, axis);
-		transform *= TransformMatrix.CreateTranslationMatrix(-vertex);
-
-		Points = (this * transform).Points;
-
-		switch(axis)
-		{
-			case Axes.X:
-				rotationX += angle;
-				break;
-
-			case Axes.Y:
-				rotationY += angle;
-				break;
-
-			case Axes.Z:
-				rotationZ += angle;
-				break;
-		}
-	}
-
-	public void Rotate(float angle, Axes axis, RotationDirection direction, Vertex vertex)
-	{
-		if (direction == RotationDirection.CounterClockwise)
-			Rotate(angle, axis, vertex);
-		else
-			Rotate(-angle, axis, vertex);
-	}
 }
--- Rain.Engine/Geometry/Triangle.cs
+++ Rain.Engine/Geometry/Triangle.cs
@@ -6,57 +6,13 @@ namespace Rain.Engine.Geometry;
 /// <summary>
 /// A class for creating and manipulating triangles.
 /// </summary>
-public class Triangle : ISpacial, ITwoDimensional
+public class Triangle : TwoDimensionalBase
 {
-	private float width;
+	public override uint[] Elements => new uint[] { 0, 1, 2 };
 
-	private float height;
+	public override Point[] Points { get; set; }
 
-	private float rotationX = 0;
-
-	private float rotationY = 0;
-
-	private float rotationZ = 0;
-
-	public uint[] Elements => new uint[] { 0, 1, 2 };
-
-	public Point[] Points { get; private set; }
-
-	public Vertex Location
-	{
-		get => GetCenterVertex();
-		set => Translate(value.X - Location.X, value.Y - Location.Y, value.Z - Location.Z);
-	}
-
-	public float Width
-	{
-		get => width;
-		set => Scale(value / width, 1);
-	}
-
-	public float Height 
-	{ 
-		get => height;
-		set => Scale(1, value / height); 
-	}
-
-	public float RotationX 
-	{ 
-		get => rotationX; 
-		set => Rotate(value / rotationX, Axes.X); 
-	}
-
-	public float RotationY
-	{ 
-		get => rotationY; 
-		set => Rotate(value / rotationY, Axes.Y); 
-	}
-
-	public float RotationZ 
-	{ 
-		get => rotationZ;
-		set => Rotate(value / rotationZ, Axes.Z);
-	}
+	public override int Sides { get => 3; }
 
 	/// <summary>
 	/// Creates a new <c>Triangle</c> from an array of <c>Point</c> objects.
@@ -65,14 +21,13 @@ public class Triangle : ISpacial, ITwoDimensional
 	/// <param name="points">
 	/// An array of <c>Points</c>s representing a triangle.
 	/// </param>
-	public Triangle(Point[] points)
+	public Triangle(Point[] points) :
+		base((float)points[0].GetDistanceBetween(points[1]), (float)points[0].Vertex.GetMidPoint(points[1].Vertex).GetDistanceBetween(points[2].Vertex), 0.0f, 0.0f, 0.0f)
 	{
 		if (points.Length != 3)
 			throw new Exception($"{nameof(points)} is not length 3 (Given length: {points.Length}).");
 
 		Points = points;
-		width = (float)points[0].GetDistanceBetween(points[1]);
-		height = (float)points[0].Vertex.GetMidPoint(points[1].Vertex).GetDistanceBetween(points[2].Vertex);
 	}
 
 	/// <summary>
@@ -90,7 +45,8 @@ public class Triangle : ISpacial, ITwoDimensional
 	/// <param name="height">
 	/// The <c>Triangle</c>'s height.
 	/// </param>
-	public Triangle(Vertex location, float width, float height)
+	public Triangle(Vertex location, float width, float height) :
+		base(width, height, 0.0f, 0.0f, 0.0f)
 	{
 		Points = new Point[]
 		{
@@ -98,9 +54,6 @@ public class Triangle : ISpacial, ITwoDimensional
 			new(new(location.X + width, location.Y, location.Z), new(255, 255, 255), new(1.0f, 0.0f)),
 			new(new(location.X, location.Y + height, location.Z), new(255, 255, 255), new(0.0f, 1.0f)),
 		};
-
-		this.width = width;
-		this.height = height;
 	}
 
 	/// <summary>
@@ -122,7 +75,8 @@ public class Triangle : ISpacial, ITwoDimensional
 	/// <param name="color">
 	/// The <c>Triangle</c>'s color.
 	/// </param>
-	public Triangle(Vertex location, float width, float height, Color color)
+	public Triangle(Vertex location, float width, float height, Color color) :
+		base(width, height, 0.0f, 0.0f, 0.0f)
 	{
 		Points = new Point[]
 		{
@@ -130,119 +84,15 @@ public class Triangle : ISpacial, ITwoDimensional
 			new(new(location.X + width, location.Y, location.Z), color, new(1.0f, 0.0f)),
 			new(new(location.X, location.Y + height, location.Z), color, new(0.0f, 1.0f)),
 		};
-
-		this.width = width;
-		this.height = height;
 	}
 
-	private Triangle(Triangle triangle)
+	private Triangle(Triangle triangle) :
+		base(triangle.Width, triangle.Height, triangle.RotationX, triangle.RotationY, triangle.RotationZ)
 	{
-		width = triangle.width;
-		height = triangle.height;
-
-		rotationX = triangle.rotationX;
-		rotationY = triangle.rotationY;
-		rotationZ = triangle.rotationZ;
-
 		Points = new Point[triangle.Points.Length];
 		triangle.Points.CopyTo(Points, 0);
 	}
 
-	public double GetDistanceBetween(ISpacial other)
-		=> (Location - other.Location).Maginitude;
-
-	public Vertex GetCenterVertex()
-	{
-		var greatestPointX = Points[0].Vertex.X;
-		var leastPointX = Points[0].Vertex.X;
-
-		var greatestPointY = Points[0].Vertex.Y;
-		var leastPointY = Points[0].Vertex.Y;
-
-		var greatestPointZ = Points[0].Vertex.Z;
-		var leastPointZ = Points[0].Vertex.Z;
-
-		for (var point = 0; point < Points.Length; point++)
-		{
-			if (Points[point].Vertex.X > greatestPointX)
-				greatestPointX = Points[point].Vertex.X;
-			else if (Points[point].Vertex.X < leastPointX)
-				leastPointX = Points[point].Vertex.X;
-
-			if (Points[point].Vertex.Y > greatestPointY)
-				greatestPointY = Points[point].Vertex.Y;
-			else if (Points[point].Vertex.Y < leastPointY)
-				leastPointY = Points[point].Vertex.Y;
-
-			if (Points[point].Vertex.Z > greatestPointZ)
-				greatestPointZ = Points[point].Vertex.Z;
-			else if (Points[point].Vertex.Z < leastPointZ)
-				leastPointZ = Points[point].Vertex.Z;
-		}
-
-		var midPointX = (greatestPointX + leastPointX) / 2;
-		var midPointY = (greatestPointY + leastPointY) / 2;
-		var midPointZ = (greatestPointZ + leastPointZ) / 2;
-
-		return new Vertex(midPointX, midPointY, midPointZ);
-	}
-
-	public void CopyTo(out ITwoDimensional triangle)
+	public override void CopyTo(out TwoDimensionalBase triangle)
 		=> triangle = new Triangle(this);
-
-	public void Translate(float x, float y, float z)
-		=> Points = (this * TransformMatrix.CreateTranslationMatrix(x, y, z)).Points;
-
-	public void Translate(Vertex vertex)
-		=> Translate(vertex.X, vertex.Y, vertex.Z);
-
-	public void Scale(float x, float y)
-	{
-		Points = (this * TransformMatrix.CreateScaleMatrix(x, y, 1)).Points;
-		width *= x;
-		height *= y;
-	}
-
-	public void Rotate(float angle, Axes axis)
-		=> Rotate(angle, axis, GetCenterVertex());
-
-	public void Rotate(float angle, Axes axis, RotationDirection direction)
-	{
-		if (direction == RotationDirection.CounterClockwise)
-			Rotate(angle, axis, GetCenterVertex());
-		else
-			Rotate(-angle, axis, GetCenterVertex());
-	}
-
-	public void Rotate(float angle, Axes axis, Vertex vertex)
-	{
-		var transform = TransformMatrix.CreateTranslationMatrix(vertex);
-		transform *= TransformMatrix.CreateRotationMatrix(angle, axis);
-		transform *= TransformMatrix.CreateTranslationMatrix(-vertex);
-
-		Points = (this * transform).Points;
-
-		switch(axis)
-		{
-			case Axes.X:
-				rotationX += angle;
-				break;
-
-			case Axes.Y:
-				rotationY += angle;
-				break;
-
-			case Axes.Z:
-				rotationZ += angle;
-				break;
-		}
-	}
-
-	public void Rotate(float angle, Axes axis, RotationDirection direction, Vertex vertex)
-	{
-		if (direction == RotationDirection.CounterClockwise)
-			Rotate(angle, axis, vertex);
-		else
-			Rotate(-angle, axis, vertex);
-	}
 }
